793746046eadcc52763e5f2c54abadc84af14149
pexcn <i@pexcn.me>
2021-02-20 21:56:45 +0800

    fix install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,6 @@
 TARGET_HOME=$(eval echo "~$TARGET_USER")
 
 IGNORE_FILES=".git README.md install.sh"
-find . -maxdepth 1 ! -path . $(printf " ! -name %s" $IGNORE_FILES) -print0 | xargs -0 chown -R $TARGET_USER.$TARGET_USER
 find . -maxdepth 1 ! -path . $(printf " ! -name %s" $IGNORE_FILES) -print0 | xargs -0 cp -t $TARGET_HOME
 
 echo "Install user: $TARGET_USER"

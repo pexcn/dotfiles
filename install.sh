bb4806620421182340ce6226b94f4977059f7d55
pexcn <i@pexcn.me>
2022-04-19 17:55:30 +0800

    fix: copy with -a

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ TARGET_HOME=$(eval echo "~$TARGET_USER")
 
 IGNORE_FILES=".git README.md install.sh"
 find . -maxdepth 1 ! -path . $(printf " ! -name %s" $IGNORE_FILES) -print0 | xargs -0 chown -R $TARGET_USER:$TARGET_USER
-find . -maxdepth 1 ! -path . $(printf " ! -name %s" $IGNORE_FILES) -exec cp {} $TARGET_HOME \;
+find . -maxdepth 1 ! -path . $(printf " ! -name %s" $IGNORE_FILES) -exec cp -a {} $TARGET_HOME \;
 
 echo "Install user: $TARGET_USER"
 echo "Install path: $TARGET_HOME"

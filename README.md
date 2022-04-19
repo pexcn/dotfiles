52b3e084087b8090d46a2c8b9768e2d9e19d4e46
pexcn <i@pexcn.me>
2022-04-19 17:50:08 +0800

    add install script

---
+++ README.md
@@ -0,0 +1,11 @@
+# dotfiles
+
+Dotfiles for PostmarketOS.
+
+## Install
+
+```bash
+git clone https://github.com/pexcn/dotfiles.git -b postmarketos && cd dotfiles
+./install.sh [user]
+cd .. && rm -r dotfiles
+```
---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+#
+# ./install.sh [user]
+#
+
+set -e
+
+[ -z "$1" ] && TARGET_USER=$USER || TARGET_USER="$1"
+TARGET_HOME=$(eval echo "~$TARGET_USER")
+
+IGNORE_FILES=".git README.md install.sh"
+find . -maxdepth 1 ! -path . $(printf " ! -name %s" $IGNORE_FILES) -print0 | xargs -0 chown -R $TARGET_USER:$TARGET_USER
+find . -maxdepth 1 ! -path . $(printf " ! -name %s" $IGNORE_FILES) -exec cp {} $TARGET_HOME \;
+
+echo "Install user: $TARGET_USER"
+echo "Install path: $TARGET_HOME"
